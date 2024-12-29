3a4fd073aae2021b1fd22e500e2575d41a5ae288
Tulsi77choudhary <tulsichoudhary421@gmail.com>
2024-12-29 19:59:21 +0530

    add file on github

---
+++ 06_Num_Maths.js
@@ -0,0 +1,45 @@
+// const score= 300
+// console.log(score);
+
+
+// const bakance= new Number(200)
+// console.log(balance);
+// console.log(num.toString().length);
+// console.log(valueOf.num);
+// console.log(num.toFixed(1));
+
+// const otherNumber = 123.9853
+
+// console.log(otherNumber.toString().length);
+// console.log(otherNumber.toPrecision(5));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//+++++++++++++++++++++++   MATHS   ++++++++++++++++++=+++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(3.5));
+
+// console.log(Math.max(4,5,2,7,1));
+// console.log(Math.min(4,5,2,7,1));
+
+console.log(Math.random()* 15 + 1);
+
+const min = 10 
+const max =20
+console.log(Math.floor (Math.random(max - min + 1)) + 1);
+
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+ 
+
+
+
+
+
+
+
