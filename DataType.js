588178a9477a6fc784c8a4d623eac1e176567d98
Tulsi77choudhary <tulsichoudhary421@gmail.com>
2025-01-01 10:43:08 +0530

    add these file

---
+++ DataType.js
@@ -0,0 +1,42 @@
+/********  Primitive
+
+7 types : String , Number , boolean, Undefined, Sysmbol,BigInt, null
+
+*/
+const score = 100
+const scoreValue = 100.3
+
+let UserEmail; // undefined value
+
+
+//(Non-Primitive) = Array, Objects,Functions
+
+//obj
+
+const heros = ["Salman","superman","Saktiman"];
+
+let obj = {
+    name: "tulsi",
+    age: 22,
+}
+
+// Function
+
+const myFunction = function (){
+console.log("Hello World");
+}
+
+console.log(typeof bit);
+
+
+// **************************************
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let name = "tulsi"
+
+let username = name
+
+console.log(username);
+name1 = "ram"
+console.log(name1);
