1ba885bbfab62d3ea62dd1ec70aa7117b2785874
Tulsi77choudhary <tulsichoudhary421@gmail.com>
2025-01-01 10:45:15 +0530

    add file

---
+++ 07_Date.js
@@ -0,0 +1,14 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getMilliseconds());
+
+console.log(myDate.toUTCString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+
+
+
+
+
+
