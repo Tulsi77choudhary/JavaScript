fb13a9f08a763c9c2fecb88b8768af0c1b538fbf
Tulsi77choudhary <tulsichoudhary421@gmail.com>
2025-01-07 11:26:53 +0530

    add a file

---
+++ Basics_01/02_Array.js
@@ -0,0 +1,9 @@
+const myHeros = ["superman","nagraj","siddhuPagi"]
+const myHeroins = ["Katrina","silpa","mannu"]
+
+// myHeros.push(myHeroins);
+// const Ner = myHeros.concat(myHeroins)
+// console.log(Ner);
+
+const all_hiros_hiroin =[...myHeros,...myHeroins]
+console.log(all_hiros_hiroin);
